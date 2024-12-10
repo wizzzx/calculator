60e594c8c78d31e8c3057a0c13f012b1d6392862
wizzzx <alexkryukovzzx@gmail.com>
2024-12-10 17:49:08 +0300

    initial commit (layout)

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function add(firstNumber, secondNumber) {
+    const result = firstNumber + secondNumber;
+    return result;
+}
+
+function subtract(firstNumber, secondNumber) {
+    const result = firstNumber - secondNumber;
+    return result;
+}
+
+function multiply(firstNumber, secondNumber) {
+    const result = firstNumber * secondNumber;
+    return result;
+}
+
+function divide(firstNumber, secondNumber) {
+    const result = firstNumber / secondNumber;
+    return result;
+}
+
+let firstInput;
+let secondInput;
+let operator;
+
+function operate(firstInput, secondInput, operator) {
+    if (operator.id === 'plus') {}
+}
 
