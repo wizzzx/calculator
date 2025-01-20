a482b46769f5f0f37539ff595faba6c711a0c2a6
wizzzx <alexkryukovzzx@gmail.com>
2025-01-20 17:16:01 +0300

    mostly done, need some styling and delete functionality

--- index.js
+++ index.js
@@ -2,29 +2,81 @@ const calculator = document.querySelector('#calculator');
 const display = document.querySelector('#display');
 const buttonPanel = document.querySelector('#button-panel');
 const buttons = document.querySelectorAll('button');
+const equals = document.querySelector('#equals');
+const del = document.querySelector('#del');
+const reset = document.querySelector('#reset');
+const displayResult = document.querySelector('#display');
 
-let a
-let b
+let a = ''
+let b = ''
 let operator
 
-function renderOnDisplay(value) {
-    const input = document.createElement('span');
-    input.textContent = value;
-    display.appendChild(input);
+function operate(a, b, operator) {
+    if (operator === '+') {
+        return a + b
+    } else if (operator === '-') {
+        return a - b
+    } else if (operator === '*') {
+        return a * b
+    } else if (operator === '/') {
+        return a / b
+    }
 }
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         const buttonValue = e.target.value
-        renderOnDisplay(buttonValue);
+        if (!['+', '-', '*', '/'].includes(buttonValue) && !operator) {
+            a = a + buttonValue
+            const firstInput = document.createElement('p')
+            firstInput.textContent = buttonValue
+            display.appendChild(firstInput)
+            a = Number(a)
+            console.log('a=', a, typeof a)
+            return a
+        } else if (!['+', '-', '*', '/'].includes(buttonValue) && operator) {
+            b = b + buttonValue
+            const secondInput = document.createElement('p')
+            secondInput.textContent = buttonValue
+            display.appendChild(secondInput)
+            b = Number(b)
+            console.log('b=', b)
+            return b
+        } else if (['+', '-', '*', '/'].includes(buttonValue) && !operator) {
+            operator = buttonValue
+            const operatorInput = document.createElement('p')
+            operatorInput.textContent = operator
+            display.appendChild(operatorInput)
+            console.log(operator)
+        } else {
+            buttons.forEach((button) => {
+                button.addEventListener('click', (e) => {
+
+                })
+            })
+        }
     })
 })
 
+equals.addEventListener('click', (e) => {
+    const result = operate(a, b, operator)
+    const resultInput = document.createElement("p")
+    resultInput.textContent = result
+    displayResult.appendChild(resultInput)
+    console.log(result)
+})
 
+reset.addEventListener('click', (e) => {
+    a = ''
+    b = ''
+    operator = ''
+    display.innerHTML = ''
+})
 
-function operate(a, b, operator) {
+del.addEventListener('click', (e) => {
+    if (a !== '' && !operator) {
 
-}
-
+    }
+})
 
 
